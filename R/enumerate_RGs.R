47d2473bee04be3a04486355fc6ce54f83befb59
artaylor85 <aimee.r.taylor.85@gmail.com>
2022-05-10 18:03:18 +0200

    clean code

--- R/enumerate_RGs.R
+++ R/enumerate_RGs.R
@@ -1,113 +1,115 @@
 # generate_all_models_3Ts() copied from the following link
 # https://github.com/jwatowatson/RecurrentVivax/blob/master/Genetic_Model/iGraph_functions.R
 # The function generate_all_models_3Ts() was written by James Watson and edited by Aimee Taylor
-# Sort documentation
-enumerate_RGs = function(MOIs) {
-  
-  # Hard code relationship types to satisfy the test_transitive function 
-  relationship_types = c(stranger = 0, sibling = 0.5, clone = 1) 
+# Sort documentation and add tests
+enumerate_RGs <- function(MOIs) {
+
+  # Hard code relationship types to satisfy the test_transitive function
+  relationship_types <- c(stranger = 0, sibling = 0.5, clone = 1)
   time_point_count <- length(MOIs) # Number of time points
   gs_count <- sum(MOIs) # Number of genotypes
-  
+
   # Check if feasible to enumerate RGs
   if (any(MOIs == 0)) stop("Zero-valued MOIs are not supported")
   if (gs_count > 6 | time_point_count > 3) stop("Sorry, too many RGs")
   if (gs_count <= 1) stop("Sorry, no RGs")
-  
+
   # Enumerate indices for pairs of time points, etc.
   if (time_point_count > 1) {
-    time_point_ijs <- gtools::combinations(n = time_point_count, 
-                                           r = 2, 
-                                           v = 1:time_point_count)   
+    time_point_ijs <- gtools::combinations(n = time_point_count,
+                                           r = 2,
+                                           v = 1:time_point_count)
   } else {
-    time_point_ijs <- matrix(1,1,2)
+    time_point_ijs <- matrix(1, 1, 2)
   }
-  
+
   time_point_pair_count <- nrow(time_point_ijs) # Number of pairs of time points
-  gs <- paste0("g",1:gs_count) # Genotype names
+  gs <- paste0("g", 1:gs_count) # Genotype names
   ts_per_gs <- rep(1:time_point_count, MOIs) # List time point indices per genotype
-  
+
   # List genotypes per time point and time point pair
-  gs_per_ts <- split(gs, ts_per_gs) 
-  gs_per_t_pairs <- lapply(1:time_point_pair_count, function(t_pair) { 
-    list(gs_per_ts[[time_point_ijs[t_pair, 1]]], 
+  gs_per_ts <- split(gs, ts_per_gs)
+  gs_per_t_pairs <- lapply(1:time_point_pair_count, function(t_pair) {
+    list(gs_per_ts[[time_point_ijs[t_pair, 1]]],
          gs_per_ts[[time_point_ijs[t_pair, 2]]])
   })
-  
+
   # Enumerate all combinations of intra time-point relationships
-  intra_relationship_types <- relationship_types[setdiff(names(relationship_types), "clone")] 
+  intra_relationship_types <- relationship_types[setdiff(names(relationship_types), "clone")]
   intra_relationships <- lapply(MOIs, function(MOI) {
     if (MOI > 1) {
-      gtools::permutations(n = length(intra_relationship_types), 
-                           r = choose(MOI,2), 
-                           v = intra_relationship_types, 
+      gtools::permutations(n = length(intra_relationship_types),
+                           r = choose(MOI, 2),
+                           v = intra_relationship_types,
                            repeats.allowed = T)
-    } else {matrix(0,1,1)}
+    } else {
+      matrix(0, 1, 1)
+    }
   })
-  
+
   # Enumerate all combinations of inter time-point relationships
   if (time_point_count > 1) {
     inter_relationships <- lapply(1:time_point_pair_count, function(t_pair) {
       as.matrix(expand.grid(rep(list(relationship_types), prod(MOIs[time_point_ijs[t_pair,]]))))
     })
-    inter_count <- sapply(inter_relationships, nrow) # Numbers of inter_relationships to permute 
+    inter_count <- sapply(inter_relationships, nrow) # Numbers of inter_relationships to permute
   } else {
     inter_count <- c()
   }
-  
+
   # Compute the number of not-necessarily transitive relationship graphs
-  intra_count <- sapply(intra_relationships, nrow) # Numbers of intra_relationships to permute 
+  intra_count <- sapply(intra_relationships, nrow) # Numbers of intra_relationships to permute
   all_counts <- c(intra_count, inter_count)
   all_perms <- as.matrix(expand.grid(sapply(all_counts, function(x) 1:x))) # Matrix of permutations
   total_count <- prod(all_counts) # Total number of permutations
   if (nrow(all_perms) != total_count) stop("Problem with the not-necessarily transitive RG count")
-  print(paste('number of not-necessarily transitive graphs is', total_count))
-  
+  print(paste("number of not-necessarily transitive graphs is", total_count))
+
   # Allocate adjacency matrices
   adj_all <- array(NA, dim = rep(gs_count, 2), dimnames = list(gs,gs))
   intra_adjs <- lapply(MOIs, function(MOI) array(NA, dim = rep(MOI, 2)))
-  
+
   # Create progress bar and count of transitive RGs
-  pbar = txtProgressBar(min = 1, max = total_count)
-  transitive_RGs = list()
+  pbar <- txtProgressBar(min = 1, max = total_count)
+  transitive_RGs <- list()
   transitive_i <- 1
-  
+
   for(perm_i in 1:total_count) {
-    
+
     setTxtProgressBar(pbar, perm_i)
-    
+
     # Extract permutation indices
-    intra_perm <- all_perms[perm_i,1:time_point_count]
-    inter_perm <- all_perms[perm_i,-(1:time_point_count)]
-    
+    intra_perm <- all_perms[perm_i, 1:time_point_count]
+    inter_perm <- all_perms[perm_i, - (1:time_point_count)]
+
     # Populate adj_all with intra-relationships
     for(t in 1:time_point_count) {
-      intra_adjs[[t]][lower.tri(intra_adjs[[t]])] <- intra_relationships[[t]][intra_perm[t],]
+      intra_adjs[[t]][lower.tri(intra_adjs[[t]])] <- intra_relationships[[t]][intra_perm[t], ]
       adj_all[gs_per_ts[[t]], gs_per_ts[[t]]] <- intra_adjs[[t]]
     }
-    
+
     # Populate lower tri of all_adj with between time-point relationships
     if (time_point_pair_count >= 1) {
-      for(t_pair in 1:time_point_pair_count) {
-        adj_all[gs_per_t_pairs[[t_pair]][[2]], 
-                gs_per_t_pairs[[t_pair]][[1]]] <- inter_relationships[[t_pair]][inter_perm[t_pair],]
+      for (t_pair in 1:time_point_pair_count) {
+        adj_all[gs_per_t_pairs[[t_pair]][[2]],
+                gs_per_t_pairs[[t_pair]][[1]]] <- inter_relationships[[t_pair]][inter_perm[t_pair], ]
       }
     }
-    
+
     # Convert adjacency matrix into an igraph item
-    RG = igraph::graph_from_adjacency_matrix(adjmatrix = adj_all, 
-                                             mode = 'lower', 
-                                             diag = F, 
-                                             weighted = 'weight')
-    # Add a time-point attribute 
-    RG = igraph::set_vertex_attr(RG, "timepoint", value = ts)
-    
+    RG <- igraph::graph_from_adjacency_matrix(adjmatrix = adj_all,
+                                             mode = "lower",
+                                             diag = F,
+                                             weighted = T)
+    # Add a time-point attribute
+    RG <- igraph::set_vertex_attr(RG, "timepoint", value = ts_per_gs)
+
     # Test transitivity and store if transitive
     if (test_transitive(G = RG)) {
       transitive_RGs[[transitive_i]] <- RG
       transitive_i <- transitive_i + 1
     }
   }
-  
+
   return(transitive_RGs)
 }
--- R/test_transitive.R
+++ R/test_transitive.R
@@ -3,37 +3,37 @@
 # This function was written by James Watson and edited by Aimee Taylor
 
 #' This implements the viable graph brute-force search algorithm described in the appendix of
-#' Taylor & Watson et al. Nature communications 10.1 (2019). 
+#' Taylor & Watson et al. Nature communications 10.1 (2019).
 #' As is, this function assumes there are only three relationships: strangers, siblings and clones
 #' represented by values 0, 0.5 and 1, respectively. As such, all connected components of the relationship
 #' graph must be cliques (in other words, the graph must be a cluster graph) and, among
 #' cliques of size three, an edge weight sum of 2.5 is indicative of a intransitive clone + clone + sibling trio
-test_transitive = function(G){
-  
-  INCORRECT <- FALSE # Presume correct until proven incorrect
-  
+test_transitive <- function(G){
+
+  transitive <- TRUE # Presume correct until proven incorrect
+
   # First check that all connected components are cliques
   S <- igraph::components(graph = G)
-  for(k in 1:S$no){
-    G_sub = igraph::induced_subgraph(graph = G, vids = igraph::vertex_attr(G)$name[S$membership==k])
-    MC = igraph::max_cliques(graph = G_sub, min = igraph::vcount(G_sub)) 
-    if (length(MC) == 0) INCORRECT = TRUE
+  for (k in 1:S$no) {
+    G_sub <- igraph::induced_subgraph(graph = G, vids = igraph::vertex_attr(G)$name[S$membership == k])
+    MC <- igraph::max_cliques(graph = G_sub, min = igraph::vcount(G_sub))
+    if (length(MC) == 0) transitive <- FALSE
   }
-  
-  # If all connected components are cliques, then iterate through all cliques of size 3
-  if(!INCORRECT){ 
-    all_trios = igraph::cliques(graph = G, min = 3, max=3)
-    K = length(all_trios)
+
+  # If all connected components are cliques, iterate through cliques of size 3
+  if (transitive) {
+    all_trios <- igraph::cliques(graph = G, min = 3, max = 3)
+    K <- length(all_trios)
     if (K > 0) {
-      i = 1
-      while(!INCORRECT & i <= K){ # stop searching as soon as we find it's incorrect
+      i <- 1
+      while (transitive & i <= K) { # stop searching when incorrect
         G_sub <- igraph::induced_subgraph(graph = G, vids = all_trios[[i]])
-        Score_sub <- sum(igraph::E(G_sub)$weight) # the sum of the edges tells us if it's correct
-        if (Score_sub == 2.5) INCORRECT <- TRUE  # 2.5 is the only possible incorrect score so we reject
-        i <- i+1
+        score_sub <- sum(igraph::E(G_sub)$weight) # the sum of the edges tells us if it's correct
+        if (score_sub == 2.5) transitive <- FALSE  # 2.5 is the only possible incorrect score so we reject
+        i <- i + 1
       }
     }
   }
-  
-  ifelse(INCORRECT, FALSE, TRUE)
+
+  return(transitive)
 }
