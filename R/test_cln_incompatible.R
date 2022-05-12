06561233a0923dac6480ddb15ed62a1bec410980
artaylor85 <aimee.r.taylor.85@gmail.com>
2022-05-12 16:37:01 +0200

    Changed to test clone compatible

--- R/test_cln_incompatible.R
+++ R/test_cln_incompatible.R
@@ -1,4 +1,4 @@
-#' Check if Pr_yn_G = 0 due to a clonal edge between non-identical genotypes
+#' Check if Pr_yn_G > 0 due to a clonal edge between non-identical genotypes
 #'
 #' Internal check function
 #'
@@ -6,18 +6,19 @@
 #'   \code{test_cln_incompatible} at
 #'   \url{https://github.com/jwatowatson/RecurrentVivax/blob/master/Genetic_Model/iGraph_functions.R}.
 #'
-test_cln_compatible <- function(A, vertex_data_matrix){
+test_clone_compatible <- function(A, vertex_data_matrix){
 
   cln_edges = which(A == 1, arr.ind = TRUE) # Which edges are clones
   num_cln_edges = nrow(cln_edges) # Number of edges that are clones
 
-  if(num_cln_edges == 0){ # If there are no clones, return FALSE
-    return(FALSE)
-  } else { # For each clonal edge
+  if(num_cln_edges == 0){ # If there are no clones, return TRUE
+    return(TRUE)
+  } else { # For each clonal edge...
     for(i in 1:nrow(cln_edges)){
-      score = !all(vertex_data_matrix[cln_edges[i,'row'],] == vertex_data_matrix[cln_edges[i,'col'],],
-                   na.rm = T) # Doesn't count NAs as different
-      if(score){break()} # as soon as different, fail and break
+      alleles_1 <- vertex_data_matrix[cln_edges[i,'row'],]
+      alleles_2 <- vertex_data_matrix[cln_edges[i,'col'],]
+      score = all(alleles_1 == alleles_2, na.rm = T) # Doesn't count NAs as different
+      if(!score){break()} # as soon as different, fail and break
     }
     return(score)
   }
