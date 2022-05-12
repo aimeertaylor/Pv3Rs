72ae445869cbb49097630bc31518c9a75107879f
artaylor85 <aimee.r.taylor.85@gmail.com>
2022-05-12 16:39:31 +0200

    Rename to match content

--- R/test_cln_compatible.R
+++ R/test_cln_compatible.R
@@ -1,12 +1,12 @@
-#' Check if Pr_yn_G > 0 due to a clonal edge between non-identical genotypes
-#'
 #' Internal check function
+#'
+#' Check if Pr_yn_G > 0 due to a clonal edge between non-identical genotypes
 #'
 #' @section Provenance: This function was adapted from
 #'   \code{test_cln_incompatible} at
 #'   \url{https://github.com/jwatowatson/RecurrentVivax/blob/master/Genetic_Model/iGraph_functions.R}.
 #'
-test_clone_compatible <- function(A, vertex_data_matrix){
+test_cln_compatible <- function(A, vertex_data_matrix){
 
   cln_edges = which(A == 1, arr.ind = TRUE) # Which edges are clones
   num_cln_edges = nrow(cln_edges) # Number of edges that are clones
