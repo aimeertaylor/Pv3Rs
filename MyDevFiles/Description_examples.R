cec2dcabeefb29cccb825416dcd9398b1286942d
Yong See Foo <yongsee0819@yahoo.com>
2022-10-26 23:02:15 +1100

    verify examples from Pv3R description

--- MyDevFiles/Description_examples.R
+++ MyDevFiles/Description_examples.R
@@ -1,5 +1,5 @@
 library(Pv3Rs)
-library(gtools)
+library(gtools) # for rdirichlet
 
 set.seed(1)
 
@@ -7,7 +7,6 @@ causes <- c("C", "L", "I")
 alleles <- c("A", "C", "G", "T")
 n.as <- length(alleles)
 alphas <- rep(1, n.as)
-
 
 # Example 1
 
@@ -17,9 +16,10 @@ fmat <- rdirichlet(n.m, alphas)
 colnames(fmat) <- alleles
 fs <- setNames(lapply(1:n.m, function(x) fmat[x,]), paste0("m", 1:n.m))
 
-print(compute_posterior(y, fs)$marg)
-writeLines("Manual calculation:")
-print(setNames(c(0, 1/3, 2/3), causes))
+post <- compute_posterior(y, fs)
+expect <- matrix(c(0, 1/3, 2/3), ncol=3, byrow=T)
+colnames(expect) <- causes
+testthat::expect_equal(post$marg, expect)
 
 
 # Example 2
@@ -30,12 +30,15 @@ fmat <- rdirichlet(n.m, alphas)
 colnames(fmat) <- alleles
 fs <- setNames(lapply(1:n.m, function(x) fmat[x,]), paste0("m", 1:n.m))
 
-print(compute_posterior(y, fs)$marg)
-writeLines("Manual calculation:")
-print(setNames(c(2/3/(1+fs$m1['A']),
-                 1/3,
-                 2/3*fs$m1['A']/(1+fs$m1['A'])),
-               causes))
+post <- compute_posterior(y, fs)
+l.ntor <- fs$m1['A']+1
+i.ntor <- 2*fs$m1['A']
+c.ntor <- 2
+dtor <- sum(c(l.ntor, i.ntor, c.ntor))
+expect <- matrix(c(c.ntor, l.ntor, i.ntor)/dtor,
+                 ncol=3, byrow=T)
+colnames(expect) <- causes
+testthat::expect_equal(post$marg, expect)
 
 
 # Example 3
@@ -46,14 +49,15 @@ fmat <- rdirichlet(n.m, alphas)
 colnames(fmat) <- alleles
 fs <- setNames(lapply(1:n.m, function(x) fmat[x,]), paste0("m", 1:n.m))
 
-print(compute_posterior(y, fs)$marg)
-writeLines("Manual calculation:")
+post <- compute_posterior(y, fs)
 l.ntor <- (fs$m2['T']*fs$m3['G']+(fs$m2['T']+1)*(fs$m3['G']+1)/8)/3
 i.ntor <- fs$m2['T']*fs$m3['G']
-print(setNames(c(0,
-                 l.ntor/(l.ntor+i.ntor),
-                 i.ntor/(l.ntor+i.ntor)),
-               causes))
+c.ntor <- 0
+dtor <- sum(c(l.ntor, i.ntor, c.ntor))
+expect <- matrix(c(c.ntor, l.ntor, i.ntor)/dtor,
+                 ncol=3, byrow=T)
+colnames(expect) <- causes
+testthat::expect_equal(post$marg, expect)
 
 
 # Example 4
@@ -64,16 +68,15 @@ fmat <- rdirichlet(n.m, alphas)
 colnames(fmat) <- alleles
 fs <- setNames(lapply(1:n.m, function(x) fmat[x,]), paste0("m", 1:n.m))
 
-print(compute_posterior(y, fs)$marg)
-writeLines("Manual calculation:")
+post <- compute_posterior(y, fs)
 l.ntor <- (fs$m1['A']+1)*(fs$m2['T']+1)*(fs$m3['G']+1)/8 + fs$m1['A']*fs$m2['T']*fs$m3['G'] + 1
 i.ntor <- 3*fs$m1['A']*fs$m2['T']*fs$m3['G']
 c.ntor <- 3
-dtor <- l.ntor + i.ntor + c.ntor
-print(setNames(c(c.ntor/dtor,
-                 l.ntor/dtor,
-                 i.ntor/dtor),
-               causes))
+dtor <- sum(c(l.ntor, i.ntor, c.ntor))
+expect <- matrix(c(c.ntor, l.ntor, i.ntor)/dtor,
+                 ncol=3, byrow=T)
+colnames(expect) <- causes
+testthat::expect_equal(post$marg, expect)
 
 
 # Example 5
@@ -84,24 +87,20 @@ fmat <- rdirichlet(n.m, alphas)
 colnames(fmat) <- alleles
 fs <- setNames(lapply(1:n.m, function(x) fmat[x,]), paste0("m", 1:n.m))
 
-print(compute_posterior(y, fs)$marg)
-writeLines("Manual calculation:")
-l.ntor <- 17/24*fs$m1['A']*fs$m1['T']^2 + 11/16*fs$m1['A']*fs$m1['T']
-i.ntor <- 7/5*fs$m1['A']*fs$m1['T']^2 + 13/10*fs$m1['A']*fs$m1['T']
-c.ntor <- 0
-dtor <- l.ntor + i.ntor + c.ntor
-print(setNames(c(c.ntor/dtor,
-                 l.ntor/dtor,
-                 i.ntor/dtor),
-               causes))
-l.ntor <- 73/120*fs$m1['A']*fs$m1['T']^2 + 39/80*fs$m1['A']*fs$m1['T']
-i.ntor <- 3/2*fs$m1['A']*fs$m1['T']^2
-c.ntor <- 3/2*fs$m1['A']*fs$m1['T']
-dtor <- l.ntor + i.ntor + c.ntor
-print(setNames(c(c.ntor/dtor,
-                 l.ntor/dtor,
-                 i.ntor/dtor),
-               causes))
+post <- compute_posterior(y, fs)
+l1.ntor <- 17/24*fs$m1['A']*fs$m1['T']^2 + 11/16*fs$m1['A']*fs$m1['T']
+i1.ntor <- 7/5*fs$m1['A']*fs$m1['T']^2 + 13/10*fs$m1['A']*fs$m1['T']
+c1.ntor <- 0
+dtor1 <- sum(c(l1.ntor, i1.ntor, c1.ntor))
+l2.ntor <- 73/120*fs$m1['A']*fs$m1['T']^2 + 39/80*fs$m1['A']*fs$m1['T']
+i2.ntor <- 3/2*fs$m1['A']*fs$m1['T']^2
+c2.ntor <- 3/2*fs$m1['A']*fs$m1['T']
+dtor2 <- sum(c(l2.ntor, i2.ntor, c2.ntor))
+expect <- matrix(c(c(c1.ntor, l1.ntor, i1.ntor)/dtor1,
+                   c(c2.ntor, l2.ntor, i2.ntor)/dtor2),
+                 ncol=3, byrow=T)
+colnames(expect) <- causes
+testthat::expect_equal(post$marg, expect)
 
 
 # Example 7
@@ -112,13 +111,12 @@ fmat <- rdirichlet(n.m, alphas)
 colnames(fmat) <- alleles
 fs <- setNames(lapply(1:n.m, function(x) fmat[x,]), paste0("m", 1:n.m))
 
-print(compute_posterior(y, fs)$marg)
-writeLines("Manual calculation:")
+post <- compute_posterior(y, fs)
 l.ntor <- 5/18*fs$m1['A']*fs$m1['T']^2 + 1/4*fs$m1['A']*fs$m1['T']
 i.ntor <- 3/4*fs$m1['A']*fs$m1['T']^2
 c.ntor <- 3/8*fs$m1['A']*fs$m1['T']
-dtor <- l.ntor + i.ntor + c.ntor
-print(setNames(c(c.ntor/dtor,
-                 l.ntor/dtor,
-                 i.ntor/dtor),
-               causes))
+dtor <- sum(c(l.ntor, i.ntor, c.ntor))
+expect <- matrix(c(c.ntor, l.ntor, i.ntor)/dtor,
+                 ncol=3, byrow=T)
+colnames(expect) <- causes
+testthat::expect_equal(post$marg, expect)
