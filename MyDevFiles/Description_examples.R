5fa10a64bb79d4c84cdd3b896db4318106c12b0f
Aimee  TAYLOR <ataylor@ml22-0157.corp.pasteur.fr>
2022-11-03 15:45:06 +0100

    Add all examples but 8; example 9 currently inconsistent

--- MyDevFiles/Description_examples.R
+++ MyDevFiles/Description_examples.R
@@ -2,34 +2,29 @@ library(Pv3Rs)
 library(gtools) # for rdirichlet
 
 set.seed(1)
-
 causes <- c("C", "L", "I")
 alleles <- c("A", "C", "G", "T")
 n.as <- length(alleles)
-alphas <- rep(1, n.as)
+alphas <- rep(1, n.as) # Dirichlet param. vector
 
 # Example 1
-
 y <- list(list(m1="A"), list(m1="T"))
 n.m <- 1
 fmat <- rdirichlet(n.m, alphas)
 colnames(fmat) <- alleles
 fs <- setNames(lapply(1:n.m, function(x) fmat[x,]), paste0("m", 1:n.m))
-
 post <- compute_posterior(y, fs)
 expect <- matrix(c(0, 1/3, 2/3), ncol=3, byrow=T)
 colnames(expect) <- causes
 testthat::expect_equal(post$marg, expect)
 
 
-# Example 2
-
+# Example 2, taking common denominator 3*(fs$m1["A"] + 1))
 y <- list(list(m1="A"), list(m1="A"))
 n.m <- 1
 fmat <- rdirichlet(n.m, alphas)
 colnames(fmat) <- alleles
 fs <- setNames(lapply(1:n.m, function(x) fmat[x,]), paste0("m", 1:n.m))
-
 post <- compute_posterior(y, fs)
 l.ntor <- fs$m1['A']+1
 i.ntor <- 2*fs$m1['A']
@@ -48,7 +43,6 @@ n.m <- 3
 fmat <- rdirichlet(n.m, alphas)
 colnames(fmat) <- alleles
 fs <- setNames(lapply(1:n.m, function(x) fmat[x,]), paste0("m", 1:n.m))
-
 post <- compute_posterior(y, fs)
 l.ntor <- (fs$m2['T']*fs$m3['G']+(fs$m2['T']+1)*(fs$m3['G']+1)/8)/3
 i.ntor <- fs$m2['T']*fs$m3['G']
@@ -80,7 +74,6 @@ testthat::expect_equal(post$marg, expect)
 
 
 # Example 5
-
 y <- list(list(m1="A"), list(m1="T"), list(m1="T"))
 n.m <- 1
 fmat <- rdirichlet(n.m, alphas)
@@ -103,14 +96,21 @@ colnames(expect) <- causes
 testthat::expect_equal(post$marg, expect)
 
 
-# Example 7
+# Example 6 (hand computed likelihood / Pv3R computed posterior)
+y <- list(list(m1="A"), list(m1="A"), list(m1="A"))
+post <- compute_posterior(y, fs)
+ll_liklihood <- fs$m1['A']^3
+cc_liklihood <- fs$m1['A']
+testthat::expect_equal(ll_liklihood/post$joint["II"],
+                       cc_liklihood/post$joint["CC"])
 
+
+# Example 7
 y <- list(list(m1=c("A","T")), list(m1="T"))
 n.m <- 1
 fmat <- rdirichlet(n.m, alphas)
 colnames(fmat) <- alleles
 fs <- setNames(lapply(1:n.m, function(x) fmat[x,]), paste0("m", 1:n.m))
-
 post <- compute_posterior(y, fs)
 l.ntor <- 5/18*fs$m1['A']*fs$m1['T']^2 + 1/4*fs$m1['A']*fs$m1['T']
 i.ntor <- 3/4*fs$m1['A']*fs$m1['T']^2
@@ -120,3 +120,31 @@ expect <- matrix(c(c.ntor, l.ntor, i.ntor)/dtor,
                  ncol=3, byrow=T)
 colnames(expect) <- causes
 testthat::expect_equal(post$marg, expect)
+
+
+# Example 9 -  wrong
+y <- list(list(m1 = c("A","T"), m2 = "T", m3 = c("C", "G")),
+          list(m1 = "T", m2 = "T", m3 = "C"))
+n.m <- 3
+fmat <- rdirichlet(n.m, alphas)
+colnames(fmat) <- alleles
+fs <- setNames(lapply(1:n.m, function(x) fmat[x,]), paste0("m", 1:n.m))
+post <- compute_posterior(y, fs)
+l_ntor <- (1/9)*(2* fs$m1["T"]*fs$m2["T"]^2*fs$m3["C"] +  3*fs$m2["T"] + 1 +
+             (3/2)* fs$m1["T"]*(fs$m2["T"]^2 + fs$m2["T"])*fs$m3["C"] +
+             (1/2)* fs$m1["T"]*(fs$m2["T"]^2 + fs$m2["T"])*(fs$m3["C"] + 1) +
+             (1/2)*(fs$m1["T"] + 1)*(fs$m2["T"]^2 + fs$m2["T"])*fs$m3["C"] +
+             (1/2)*(fs$m1["T"] + 1)*(fs$m2["T"]^2 + fs$m2["T"])*(fs$m3["C"] + 1))
+i_ntor <- (1/2)*fs$m1["T"]*fs$m2["T"]*fs$m3["C"]*(3*fs$m2["T"] + 1)
+c_ntor <- (1/8)*(3*fs$m2["T"] + 1)
+
+dtor <- sum(c(l_ntor, i_ntor, c_ntor))
+
+expect <- matrix(c(c_ntor, l_ntor, i_ntor)/dtor,
+                 ncol=3, byrow=T)
+colnames(expect) <- causes
+testthat::expect_equal(post$marg, expect)
+
+
+
+
