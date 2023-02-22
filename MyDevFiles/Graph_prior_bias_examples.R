98b1359803b5070091296f5c28b5f58b94636344
Aimee  TAYLOR <ataylor@ml22-0157.corp.pasteur.fr>
2023-02-22 19:14:01 +0100

    Examples of unwanted effect on posterior of prior on graphs

--- MyDevFiles/Graph_prior_bias_examples.R
+++ MyDevFiles/Graph_prior_bias_examples.R
@@ -1,60 +1,57 @@
-# Marginals are not the same give same data
+###############################################################################
+# Conclusion: the marginal probability that the first recurrence is a
+# reinfection increases as the graph grows only if there are data (example 4);
+# otherwise, it decreases (examples 1,2,3). Also, marginal probabilities of the
+# different recurrences differ even when they all have the same data (example 4).
+#
+# Problem: NA seems to negate recrudescence (see first and second recurrence of
+# example 2 and 3, respectively). Is there are clause somewhere that compares
+# an allele character to NA and negates recrudescence because not equal
+#
+# To-do list:
+# Add an warning for recurrences with no data having non-zero marginal probabilities
+# Integrate examples into compute_posterior documentation
+################################################################################
+
 # When not in development mode
 # devtools::install_github("aimeertaylor/Pv3Rs", build_vignettes = TRUE)
 library(Pv3Rs)
-library(gtools) # For rdirichlet
-set.seed(1) # Given rdirichlet
-
-
-# Set up
-causes <- c("C", "L", "I") # Possible causes of recurrence
-alleles <- c("A", "T", "C", "G") # Possible alleles
-return_freqs <- function(num_markers, alleles) {
-  n.as <- length(alleles) # Number of possible alleles
-  alphas <- rep(1, n.as) # Dirichlet param. vector
-  setNames(lapply(1:num_markers, function(x) setNames(rdirichlet(1, alphas)[1,], alleles)),
-           paste0("m", 1:num_markers)) # List of allele frequencies
-}
-
-#===============================================================================
-# Example: the size of the graph changes the marginal posterior of the first recurrence
-# Format problem in first instance
-# in second
-#===============================================================================
-# List of allele frequencies
-fs <- return_freqs(num_markers = 1, alleles)
-
-# Data for multiple scenarios: different numbers of recurrences but data only on the first
-ys <- list(list(list(m1 = "T"), list(m1 = "T")),  # Single recurrence
-           list(list(m1 = "T"), list(m1 = "T"), list(m1 = NA)), # Two recurrences
-           list(list(m1 = "T"), list(m1 = "T"), list(m1 = NA), list(m1 = NA))) # Three recurrences
-
-# Computation
-lapply(ys, function(y) {post <- compute_posterior(y, fs); post$marg})
-
-# Data for multiple scenarios: different numbers of recurrences but data only on the first
-ys <- list(list(m1 = "T", m1 = "T"),  # Single recurrence
-           list(m1 = "T", m1 = "T", m1 = NA), # Two recurrences
-           list(m1 = "T", m1 = "T", m1 = NA, m1 = NA))  # Three recurrences
-
-# Computation
-lapply(ys, function(y) {post <- compute_posterior(y, fs); post$marg})
+fs <- list(m1 = setNames(0.25, "A")) # List of allele frequencies
 
 
 #===============================================================================
-# Example showing how the marginals are not the same given the same data
+# Example 1: the size of the graph changes and no data whatsoever
 #===============================================================================
-# Data for a single scenario: four recurrences, each with the same data
-y <- list(list(m1 = "T", m2 = "T", m3 = "C"), #0 initial infection
-          list(m1 = "T", m2 = "T", m3 = "C"), #1 first recurrence
-          list(m1 = "T", m2 = "T", m3 = "C"), #2 second recurrence
-          list(m1 = "T", m2 = "T", m3 = "C"), #3 third recurrence
-          list(m1 = "T", m2 = "T", m3 = "C")) #4 fourth recurrence
+ys <- list(list(list(m1 = NA), list(m1 = NA)), # 1 recurrence
+           list(list(m1 = NA), list(m1 = NA), list(m1 = NA)), # 2 recurrences
+           list(list(m1 = NA), list(m1 = NA), list(m1 = NA), list(m1 = NA))) # 3 recurrences
+lapply(ys, function(y) compute_posterior(y, fs, return.RG = T)$marg)
 
-# List of allele frequencies
-fs <- return_freqs(num_markers = length(y[[1]]), alleles)
 
-# Computation
-post <- compute_posterior(y, fs)
-post$marg
+#===============================================================================
+# Example 2: the size of the graph changes and no recurrent data
+#===============================================================================
+ys <- list(list(list(m1 = "A"), list(m1 = NA)), # 1 recurrence
+           list(list(m1 = "A"), list(m1 = NA), list(m1 = NA)), # 2 recurrences
+           list(list(m1 = "A"), list(m1 = NA), list(m1 = NA), list(m1 = NA))) # 3 recurrences
+lapply(ys, function(y) compute_posterior(y, fs, return.RG = T)$marg)
+
+
+#===============================================================================
+# Example 3: the size of the graph changes but only one recurrence with data
+#===============================================================================
+ys <- list(list(list(m1 = "A"), list(m1 = "A")), # 1 recurrence
+           list(list(m1 = "A"), list(m1 = "A"), list(m1 = NA)), # 2 recurrences
+           list(list(m1 = "A"), list(m1 = "A"), list(m1 = NA), list(m1 = NA))) # 3 recurrences
+lapply(ys, function(y) compute_posterior(y, fs, return.RG = T)$marg)
+
+
+#===============================================================================
+# Example 4: the size of the graph changes with repeat data
+#===============================================================================
+ys <- list(list(list(m1 = "A"), list(m1 = "A")),  # 1 recurrence
+           list(list(m1 = "A"), list(m1 = "A"), list(m1 = "A")), # 2 recurrences
+           list(list(m1 = "A"), list(m1 = "A"), list(m1 = "A"), list(m1 = "A"))) # 3 recurrences
+lapply(ys, function(y) compute_posterior(y, fs)$marg)
+
 
