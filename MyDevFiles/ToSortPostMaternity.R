b96e50dd588681773a8a0c3a5800d8e79cadbb08
Aimee Taylor <aimee.r.taylor.85@gmail.com>
2023-05-27 14:56:04 +0200

    Expand notes

--- MyDevFiles/ToSortPostMaternity.R
+++ MyDevFiles/ToSortPostMaternity.R
@@ -1,3 +1,7 @@
+# =====================================================================
+# Initial thoughts on using repeat values to encode MOIs and within-sample
+# allele frequencies. To be continued after maternity leave (Nov 2023)
+
 library(Pv3Rs)
 
 # =====================================================================
@@ -8,7 +12,7 @@ fs <- list(m1=c('1'=0.78, '2'=0.14, '3'=0.07, '4'=0.005, '5' = 0.005),
 
 
 # =====================================================================
-# Using a dummy marker to encode MOIs
+# Repeat values encode MOIs: could use NAs at a dummy marker
 y0 <- list(enrol = list(m1=c('1')),
            recur = list(m1=c('1','1')))
 
@@ -17,7 +21,6 @@ y1 <- list(enrol = list(m1=c('1'), m2=NA),
 
 compute_posterior(y0, fs)
 compute_posterior(y1, fs)
-
 
 
 # =====================================================================
@@ -33,8 +36,9 @@ compute_posterior(y0, fs)
 compute_posterior(y1, fs)
 
 # =====================================================================
-# Multiple homoallelic observations at heteroallelic alleles provide a
-# rough way to encode within-sample allele frequencies
+# Multiple homoallelic observations at heteroallelic alleles provide a rough way
+# to encode within-sample allele frequencies. However, this is only viable if
+# the MOI exceeds the imbalance encoding (see example below)
 y0 <- list(enrol = list(m1=c('1','1','2'), m2=c('1','2','3')),
            recur = list(m1=c('1'), m2=c('1')))
 
@@ -47,4 +51,17 @@ y2 <- list(enrol = list(m1=c('1','2','2'), m2=c('1','2','3')),
 compute_posterior(y0, fs)
 compute_posterior(y1, fs)
 compute_posterior(y2, fs)
+
+
+# =====================================================================
+# Problem when wants to encode an imbalanced within-sample allele frequency
+# without inflating the MOI. For example, following encodes MOIs of 2 and 3,
+# incompatible with recrudescece; not MOIs of 2 and 2 with selection of c(2,1)
+# genotype from 50% to 66%, compatible with recrudescence
+
+y <- list(enrol = list(m1=c('1','2'), m2=c('1')),
+           recur = list(m1=c('1','2','2'), m2=c('1')))
+
+compute_posterior(y, fs)
+
 
