672fc148383c41f8e4a91e31563e477d16c0558d
aimeertaylor <aimee.r.taylor.85@gmail.com>
2024-08-23 13:59:25 +0100

    Merge Example_effect_RG into UnderstandingGraphBias

--- MyDevFiles/UnderstandingGraphBias.R
+++ MyDevFiles/UnderstandingGraphBias.R
@@ -2,12 +2,13 @@
 # To-do list:
 # Make this into a vignette
 # Add an warning for recurrences with no data can have non-zero marginal probabilities
+# Add examples from Thur 22nd August
 ################################################################################
 
-# When not in development mode:
-devtools::install_github("aimeertaylor/Pv3Rs", build_vignettes = FALSE)
-library(Pv3Rs)
-rm(list = ls())
+# # When not in development mode:
+# devtools::install_github("aimeertaylor/Pv3Rs", build_vignettes = FALSE)
+# library(Pv3Rs)
+# rm(list = ls())
 
 #===============================================================================
 # Examples of unwanted effects on posterior of prior on graphs
@@ -111,3 +112,44 @@ results0[[4]] - array(1/3, dim = dim(results0[[4]]))
 results1[[4]] - matrix(results1[[4]][1,], byrow = T,
                        nrow = nrow(results0[[4]]),
                        ncol = ncol(results0[[4]]))
+
+
+#============================================================================
+# Example of the effect on the posterior of the prior on relationship graphs
+# graphs using MOIs
+# What happens to the effect with more and more data?
+#============================================================================
+# Allele frequencies:
+f_A <- 0.99
+
+
+fs <- list(m1 = setNames(c(f_A, 1-f_A), c("A", "Other")))
+
+# Data (toggle between recur being "A" and NA)
+y <- list(enroll = list(m1 = "A"), recur = list(m1 = "A"))
+
+MOIs <- list(c(1,1), c(2,1), c(3,1), c(2,2), c(3,2), c(3,3))
+
+# Compute posterior probabilities and extract marginal probabilities:
+results <- do.call(rbind, lapply(MOIs, function(x) compute_posterior(y, fs, MOIs = x)$marg))
+
+# Plot 2D simplex
+pardefault <- par()
+par(mar = c(0,0,0,0))
+vertex_names <- c(C = "Recrudescence", L = "Relapse", I = "Reinfection")
+plot_simplex(v_labels = vertex_names[colnames(results)], classifcation_threshold = 0.5)
+
+# Project probabilities onto 2D simplex coordinates
+xy <- apply(results, 1, project2D)
+
+# # Plot divergence from one recurrence to four:
+# arrows(x0 = xy["x", 1], x1 = xy["x", length(MOIs)],
+#        y0 = xy["y", 1], y1 = xy["y", length(MOIs)],
+#        length = 0.05, col = "red")
+
+# Plot a point for each recurrence from one to four:
+points(x = xy["x", ], y = xy["y", ], pch = as.character(1:length(MOIs)), cex = 0.2)
+
+# Restore plotting margins
+par(mar = pardefault$mar)
+
