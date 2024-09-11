2bc17be00262d07cfd1ae11969270acf4f030aa3
Aimee  TAYLOR <ataylor@ml22-0157.corp.pasteur.fr>
2024-09-11 17:43:30 +0200

    Update

--- MyDevFiles/Example_PreprintFig.R
+++ MyDevFiles/Example_PreprintFig.R
@@ -1,65 +1,91 @@
 # ==============================================================================
-# Made up example, based on Figure 1 of the 2022 medRxiv preprint
-# Not quite right because sibs in initial infection ought to be meiotic.
-# To-do: update how the data, using recombine_parent_ids()
+# Made up example, based on modification of Figure 1 of the 2022 medRxiv preprint
 # ==============================================================================
+rm(list = ls())
+library(Pv3Rs)
+library(MCMCpack) # For rdirichlet
+library(tictoc) # For timing
 
-# Make parasite genotypes, where green-ish genotypes are yellow-blue mosaics;
-# Alleles are named by their ancestor.
-Yel_child <- c(m1 = "yellow", m2 = "yellow", m3 = "yellow", m4 = "yellow")
-YelBlu_child_1 <- c(m1 = "yellow", m2 = "yellow", m3 = "blue", m4 = "yellow")
-YelBlu_child_2 <- c(m1 = "yellow", m2 = "blue", m3 = "blue", m4 = "yellow")
-YelBlu_child_3 <- c(m1 = "yellow", m2 = "blue", m3 = "yellow", m4 = "blue")
-YelBlu_child_4 <- c(m1 = "blue", m2 = "yellow", m3 = "blue", m4 = "blue")
-Pink <- c(m1 = "hotpink", m2 = "hotpink", m3 = "hotpink", m4 = "hotpink")
-Red <- c(m1 = "red", m2 = "red", m3= "red", m4 = "red")
+# Magic numbers / quantities
+set.seed(5) # For reproducibility
+n_alleles <- 10 # Number of alleles per marker (marker cardinality)
+n_markers <- 10 # Number of markers
+n_strangers <- 4 # Number of stranger parasites
+c_param <- 1 # Dirichlet concentration parameter
+
+# Derived quantities
+alleles <- letters[1:n_alleles]
+markers <- paste0("m", 1:n_markers) # Marker names
+
+# Sample allele frequencies
+fs <- sapply(markers, function(m) {
+  sort(setNames(MCMCpack::rdirichlet(1, rep(c_param, n_alleles)), alleles), decreasing = T)
+  }, USE.NAMES = T, simplify = F)
+rm(alleles)
+
+# Sample strangers
+strangers <- sapply(1:n_strangers, function(i) {
+  sapply(markers, function(t) sample(names(fs[[t]]), size = 1, prob = fs[[t]]))
+})
+
+# Designate strangers
+parents <- strangers[, 1:2]
+
+# Map the markers to chromosomes. Assume equally sized chromosomes — reasonable
+# providing we later assume an equal number of crossovers per chromosome
+chrs_per_marker <- round(seq(0.51, 14.5, length.out = n_markers))
+
+# Sample parental allocations dependently (generates meiotic siblings)
+cs <- recombine_parent_ids(chrs_per_marker)
+
+# Construct children from parental allocations
+children <- sapply(1:n_markers, function(i) {
+  sapply(1:ncol(cs), function(j) parents[i,cs[i,j]])
+})
+colnames(children) <- markers
 
 # Make parasite infections
-initial <- rbind(Yel_child, YelBlu_child_1, YelBlu_child_2, YelBlu_child_3)
-relapse <- rbind(YelBlu_child_4, Pink)
-recrud <- rbind(YelBlu_child_4)
-reinf <- rbind(Red)
+initial <- apply(children[3:4,], 2, unique, simplify = F)
+relapse <- apply(rbind(children[1,], strangers[,3]), 2, unique, simplify = F)
+recrude <- as.list(strangers[,3])
+reinfec <- as.list(strangers[,4])
 
-# Make list of per-marker list of observed alleles
-# The model will mistakenly think there are two not four genotypes in the initial
-# infection
-y <- list(initial = apply(initial, 2, unique, simplify = F),
-          relapse = apply(relapse, 2, unique, simplify = F),
-          recrud = apply(recrud, 2, unique, simplify = F),
-          reinf = apply(reinf, 2, unique, simplify = F))
-
-# Function to draw allele frequencies that sum to one
-rdirichlet(1, alpha = rep(0.1,4))
-
-set.seed(1)
-fs <- list(
-  m1 = setNames(rdirichlet(1, alpha = rep(0.1,4)), c("yellow", "hotpink", "red", "blue")),
-  m2 = setNames(rdirichlet(1, alpha = rep(0.1,4)), c("yellow", "hotpink", "red", "blue")),
-  m3 = setNames(rdirichlet(1, alpha = rep(0.1,4)), c("yellow", "hotpink", "red", "blue")),
-  m4 = setNames(rdirichlet(1, alpha = rep(0.1,4)), c("yellow", "hotpink", "red", "blue"))
-)
+# Make data
+y <- list(initial = initial,
+          relapse = relapse,
+          recrude = recrude,
+          reinfec = reinfec)
 
 # Plot the data
-plot_data(ys = list(example = y))
+plot_data(ys = list(example = y), fs = fs, marker_annotate = F)
+
+# Reduce the number of markers evaluated
+for(n_markers_eval in c(3,5,10)){
+
+y_eval <- sapply(y, function(x) x[1:n_markers_eval], USE.NAMES = T, simplify = F)
 
 # Compute results
-post <- compute_posterior(y, fs, return.RG=TRUE, return.logp=TRUE)
-post$marg # Makes sense
-sort(post$joint, decreasing = T) # Makes sense
+post <- compute_posterior(y_eval, fs, return.RG=TRUE, return.logp=TRUE)
+
+# Extract results
+post$marg # Marginal probabilities
+which.max(post$joint) # Most likely sequence
 
 # Plot result on the simplex:
 xy <- apply(post$marg, 1, project2D)
-plot_simplex(v_labels = c("Recrudescence", "Relapse", "Reinfection"),
-             classifcation_threshold = 0.5)
-points(x = xy["x",], xy["y",], pch = 21, cex = 1.5,
+plot_simplex(v_labels = c("Recrudescence", "Relapse", "Reinfection"), 0.5)
+points(x = xy["x",], xy["y",], pch = 21, cex = 2,
        bg = c("purple", "yellow", "red", fg = "black"))
+}
 
-# Most likely graph (could colour according to the Figure)
+# Most likely graph
+pdf("~/Dropbox/Presentations_Slides_Figures_Posters/Presentations/GEM_2024/Example_graph", width = 7, height = 3)
 RGlogp <- sapply(post$RGs, function(RG) RG$logp)
 RG <- post$RGs[[which.max(RGlogp)]]
 MOIs <- determine_MOIs(y)
 gs <- paste0("g", 1:sum(MOIs))
 ts_per_gs <- rep(1:length(MOIs), MOIs)
 par(mar = c(0.5, 0.5, 0.5, 0.5))
-plot_RG(RG_to_igraph(RG, gs, ts_per_gs), edge.curved=0.25, vertex.size=20, vertex_palette = "Blues")
+plot_RG(RG_to_igraph(RG, gs, ts_per_gs), edge.curved=0, vertex.size=20, vertex_palette = "Blues")
 seqs_comp_MLE_RG <- compatible_rstrs(RG, split(gs, ts_per_gs))
+dev.off()
