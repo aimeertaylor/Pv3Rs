e9f9ecb8d9c4262aa632eeae4830149156b03e94
Aimee  TAYLOR <ataylor@ml22-0157.corp.pasteur.fr>
2024-10-01 15:08:13 +0200

    Update: all approximations match make sense

--- MyDevFiles/UnderstandingGraphBias.R
+++ MyDevFiles/UnderstandingGraphBias.R
@@ -8,7 +8,6 @@ rm(list = ls())
 
 # Simplex plot parameters
 pardefault <- par()
-vertex_names <- c(C = "Recrudescence", L = "Relapse", I = "Reinfection")
 par(mar = c(0,0,0,0))
 
 # Define function to get gs, RGs, states given RGs, and thus avoid repetition
@@ -23,14 +22,78 @@ get_graph_stuff <- function(x) {
 }
 
 
+#===============================================================================
+# Homoallelic example with recurrent data using recurrences to change graphs.
+#===============================================================================
+marker_count <- 100
+ms <- paste0("m", 1:marker_count)
+fs <- sapply(ms, FUN = function(m) c("A" = 0.01, "B" = 0.99), simplify = FALSE)
+clone <- as.list(sapply(ms, function(t) "A"))
+na.clone <- as.list(sapply(ms, function(t) NA))
+ys_match <- list(one_recurrence = list(enroll = clone, recur_match = clone),
+                 two_recurrences = list(enroll = clone, recur_match = clone,
+                                        recur2 = na.clone),
+                 three_recurrences = list(enroll = clone, recur_match = clone,
+                                          recur2 = na.clone,
+                                          recur3 = na.clone),
+                 four_recurrences = list(enroll = clone, recur_match = clone,
+                                         recur2 = na.clone,
+                                         recur3 = na.clone,
+                                         recur4 = na.clone))
+
+# ------------------------------------------------------------------------------
+# Posterior based on model
+# ------------------------------------------------------------------------------
+results <- lapply(ys_match, function(y) compute_posterior(y, fs)$marg)
+results_first <- sapply(results, function(result) result[1,])
+plot_simplex(c(C = "Recrudescence", L = "Relapse", I = "Reinfection"), 0.5)
+xy <- apply(results_first, 2, project2D)
+points(x = xy["x", ], y = xy["y", ], pch = 20, col = 1:length(ys_match), cex = 1)
+legend("right", col = 1:length(ys_match), pch = 20, pt.cex = 1, bty = "n",
+       legend = 1:length(ys_match), title = "Recurrence \n count")
+
+# ------------------------------------------------------------------------------
+# Posterior approximation based on graphs only
+# ------------------------------------------------------------------------------
+MOIs <- lapply(ys_match, determine_MOIs) # First, get MOIs
+graph_frac_first <- sapply(1:length(MOIs), FUN = function(i) {
+  stuff <- get_graph_stuff(MOIs[[i]])
+  seqs_str <- unique(unlist(stuff$CIL_gvn_RGs)) # Recurrent state sequence strings
+  seqs_chr <- do.call(rbind, sapply(seqs_str, function(x) strsplit(x, split = "")))
+  seqs_str_I_first <- seqs_str[which(seqs_chr[,1] == "I")] # sequences with reinfection first
+  seqs_str_C_first <- seqs_str[which(seqs_chr[,1] == "C")] # sequences with recrudescence first
+  RGs_C_first <- sapply(stuff$CIL_gvn_RGs, function(x) any(seqs_str_C_first %in% x))
+  num_comp <- sapply(seqs_str, function(seq) sum(sapply(stuff$CIL_gvn_RGs, function(x) seq %in% x)))
+  num_comp_RGs_C_first <- sapply(seqs_str, function(seq) {
+    sum(sapply(stuff$CIL_gvn_RGs, function(x) seq %in% x)*RGs_C_first)})
+  x <- num_comp_RGs_C_first / num_comp
+  z <- x/sum(x)
+  prob_c <- sum(z[seqs_str_C_first])
+  prob_l <- 1 - prob_c
+  c(prob_c, prob_l, 0)
+})
+xy <- apply(graph_frac_first, 2, project2D) # project onto the simplex
+points(x = xy["x", ], y = xy["y", ], col = 1:length(ys_match), cex = 2) # plot
+legend("bottom", pch = c(20, 1), pt.cex = c(1, 2), # Add legends
+       legend = c("Posterior probability", "Relative weighted graphs"), bty = "n")
+
+# ------------------------------------------------------------------------------
+# Graphs with largest likelihood are indeed those compatible with a recrudescence
+# at the first recurrence
+# ------------------------------------------------------------------------------
+# MOIs <- MOIs[[4]]
+# stuff <- get_graph_stuff(MOIs)
+# result1111  <- compute_posterior(ys_match[[4]], fs, return.logp = TRUE)
+# logps <- sapply(result1111$RGs, function(RG) RG$logp) # extract graph likelihood
+# table(logps)
+# #for(i in which(logps > -5)) plot_RG(stuff$RGs[[i]], edge.curved = 0.5)
+
+
+
+
 
 #===============================================================================
 # Homoallelic example without recurrent data using MOIs to change graphs.
-#
-# A rare homoallelic call has large effect (it limits the summation over
-# relationship graphs to those with intra-episode sibling edges).
-#
-# Approx. based on graphs only is good because the likelihood surface is flat
 # ===============================================================================
 fs = list(m1 = c("A" = 0.001, "B" = 0.999)) # Allele frequencies
 y <- list(enroll = list(m1 = c('A')), recur = list(m1 = NA)) # Data
@@ -41,7 +104,7 @@ MOIs <- list(c(2,1), c(3,1), c(2,2), c(3,2), c(3,3)) # MOIs
 # ------------------------------------------------------------------------------
 results <- sapply(MOIs, function(x) compute_posterior(y, fs, MOIs = x)$marg)
 plot_simplex(c("Recrudescence", "Relapse", "Reinfection"), 0.5) # Plot simplex
-xy <- apply(results, 1, project2D) # Project probabilities
+xy <- apply(results, 2, project2D) # Project probabilities
 points(x = xy["x", ], y = xy["y", ], pch = 20, cex = 1, col = 1:length(MOIs))
 legend("left", col = 1:length(MOIs), pch = 20, title = "MOIs", inset = 0,
        legend = sapply(MOIs, paste, collapse = " & "), bty = "n")
@@ -79,101 +142,28 @@ points(x = xy_graph_frac_hom["x", ], y = xy_graph_frac_hom["y", ],
 legend("bottom", pch = c(20, 1), pt.cex = c(1, 2), # Add legends
        legend = c("Posterior probability", "Relative weighted graphs"), bty = "n")
 
-# ------------------------------------------------------------------------------
-# Understanding approximation based on graphs only
-# ------------------------------------------------------------------------------
-MOIs <- c(3,3) # Illustrative example
-gs <- paste0("g", 1:sum(MOIs))
-ts <- 1:length(MOIs)
-ts_per_gs <- rep(ts, MOIs)
-gs_per_ts <- split(gs, ts_per_gs)
-result33  <- compute_posterior(y, fs, MOIs = MOIs, return.logp = TRUE)
-RGs <- sapply(result33$RGs, function(RG) RG_to_igraph(RG, gs, ts_per_gs))
-logps <- sapply(result33$RGs, function(RG) RG$logp) # extract logp
-
-# Make a vector of intra-infection edges by first creating a block diag. matrix
-mat_first <- array(1, dim = c(3, 3), dimnames = list(paste0("g", 1:3), paste0("g", 1:3)))
-edges_first <- igraph::as_ids(igraph::E(igraph::graph_from_adjacency_matrix(mat_first, mode = "undirected", diag = F)))
-RGs_edges <- lapply(stuff$RGs, function(RG) igraph::as_ids(igraph::E(RG))) # Get edges for every RG
-RGs_edges_first <- sapply(RGs_edges, function(RG_E) all(edges_first %in% RG_E))
-
-# Inspect likelihoods:
-if (max(logps[!(RGs_edges_first)]) < min(logps[RGs_edges_first])) {
-  writeLines("Expected-most-likely graphs are most likely ")
-  for(i in which(RGs_edges_first)) plot_RG(RGs[[i]])
-  high_logps <- table(logps[RGs_edges_first])
-  writeLines(sprintf("%s likelihood values among %s graphs", length(high_logps), sum(high_logps)))
-  print(high_logps)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#===============================================================================
-# Homoallelic example with recurrent data using recurrences to change graphs.
+# # ------------------------------------------------------------------------------
+# # Understanding approximation based on graphs only
+# # ------------------------------------------------------------------------------
+# MOIs <- c(3,3) # Illustrative example
+# stuff <- get_graph_stuff(MOIs)
+# result33  <- compute_posterior(y, fs, MOIs = MOIs, return.logp = TRUE)
+# logps <- sapply(result33$RGs, function(RG) RG$logp) # extract logp
 #
-# The marginal probability that the first recurrence is a recrudescence
-# increases at a decreasing rate as the graph grows without data. Decreasing the
-# frequency of the observed allele decreases the probability of reinfection.
+# # Make a vector of intra-infection edges by first creating a block diag. matrix
+# mat_first <- array(1, dim = c(3, 3), dimnames = list(paste0("g", 1:3), paste0("g", 1:3)))
+# edges_first <- igraph::as_ids(igraph::E(igraph::graph_from_adjacency_matrix(mat_first, mode = "undirected", diag = F)))
+# RGs_edges <- lapply(stuff$RGs, function(RG) igraph::as_ids(igraph::E(RG))) # Get edges for every RG
+# RGs_edges_first <- sapply(RGs_edges, function(RG_E) all(edges_first %in% RG_E))
 #
-# Graph expectation poor
-#===============================================================================
-# Data for different recurrence counts where only the 1st recurrence has data
-ys_match <- list(one_recurrence = list(enroll = list(m1 = "A"),
-                                       recur_match = list(m1 = "A")),
-                 two_recurrences = list(enroll = list(m1 = "A"),
-                                        recur_match = list(m1 = "A"),
-                                        recur2 = list(m1 = NA)),
-                 three_recurrences = list(enroll = list(m1 = "A"),
-                                          recur_match = list(m1 = "A"),
-                                          recur2 = list(m1 = NA),
-                                          recur3 = list(m1 = NA)),
-                 four_recurrences = list(enroll = list(m1 = "A"),
-                                         recur_match = list(m1 = "A"),
-                                         recur2 = list(m1 = NA),
-                                         recur3 = list(m1 = NA),
-                                         recur4 = list(m1 = NA)))
-fs <- list(m1 = c("A" = 0.001, "B" = 0.999)) # Allele frequencies
-results <- lapply(ys_match, function(y) suppressMessages(compute_posterior(y, fs)$marg))
-results_first <- sapply(results, function(result) result[1,]) # Extract 1st recurrence results
-plot_simplex(vertex_names, 0.5) # Plot 2D simplex
-xy <- apply(results_first, 2, project2D)
-points(x = xy["x", ], y = xy["y", ], pch = "-", col = 1:length(ys_match), cex = 1)
-
-# ------------------------------------------------------------------------------
-# Add graph expectation for a rare repeat allele
-# ------------------------------------------------------------------------------
-MOIs <- lapply(ys_match, determine_MOIs) # First, get MOIs
-graph_frac_first <- sapply(1:length(MOIs), FUN = function(i) {
-  stuff <- get_graph_stuff(MOIs[[i]])
-  # RGs compatible with recrudescence at the first recurrence
-  RGs_C_first <- sapply(stuff$CIL_gvn_RGs, function(x) "C" %in% do.call(rbind, strsplit(x, split = ""))[,1])
-  graph_frac_unnormalised <- c(frac_C = 1/sum(RGs_C_first),
-                               frac_L = 1/length(stuff$RGs),
-                               frac_I = 0)
-  graph_frac <- graph_frac_unnormalised/sum(graph_frac_unnormalised)
-  return(graph_frac)
-})
-xy <- apply(graph_frac_first, 2, project2D) # project onto the simplex
-points(x = xy["x", ], y = xy["y", ], col = 1:length(ys_match), cex = 1) # plot
-legend("right", col = 1:length(ys_match), pch = "-", pt.cex = 2, bty = "n",
-       legend = 1:length(ys_match), title = "Recurrence \n count")
-
-
+# # Inspect likelihoods:
+# if (max(logps[!(RGs_edges_first)]) < min(logps[RGs_edges_first])) {
+#   writeLines("Expected-most-likely graphs are most likely ")
+#   #for(i in which(RGs_edges_first)) plot_RG(RGs[[i]])
+#   high_logps <- table(logps[RGs_edges_first])
+#   writeLines(sprintf("%s likelihood values among %s graphs", length(high_logps), sum(high_logps)))
+#   print(high_logps)
+# }
 
 
 
@@ -186,9 +176,7 @@ legend("right", col = 1:length(ys_match), pch = "-", pt.cex = 2, bty = "n",
 
 #===============================================================================
 # Reinfection versus relapse using MOIs to change graphs.
-#
-# When data are informative, model returns the prior on graphs re-weighted
-# ===============================================================================
+#===============================================================================
 set.seed(1)
 marker_count <- 100; allele_count <- 2 # Data informativeness
 ms <- paste0("m", 1:marker_count)
@@ -198,21 +186,22 @@ fs <- sapply(ms, function(m) {
 }, USE.NAMES = TRUE, simplify = FALSE)
 stranger1 <- as.list(sapply(ms, function(t) sample(alleles, 1, prob = fs[[t]])))
 stranger2 <- as.list(sapply(ms, function(t) sample(alleles, 1, prob = fs[[t]])))
-y <- list(enrol = stranger1, recur = stranger1)
+y <- list(enrol = stranger1, recur = stranger2)
 MOIs <- list(c(1,1), c(2,1), c(3,1), c(2,2), c(3,2), c(3,3), c(4,2))
 
 # ------------------------------------------------------------------------------
 # Posterior based on full model
 # ------------------------------------------------------------------------------
 results <- sapply(MOIs, function(x) compute_posterior(y, fs, MOIs = x)$marg)
-plot_simplex(vertex_names, 0.5); xy <- apply(results, 2, project2D)
+plot_simplex(c(C = "Recrudescence", L = "Relapse", I = "Reinfection"), 0.5)
+xy <- apply(results, 2, project2D)
 points(x = xy["x", ], y = xy["y", ], col = 1:length(MOIs), pch = 20)
 legend("left", bty = "n", col = 1:length(MOIs), pch = 20, title = "MOIs",
        legend = sapply(MOIs, paste, collapse = " & "))
 
-# ------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # Posterior approximation based on graphs only: prior on graphs re-weighted
-# ------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 graph_frac_IL <- sapply(1:length(MOIs), FUN = function(i) {
   stuff <- get_graph_stuff(MOIs[[i]])
   RGs_I <- sapply(stuff$CIL_gvn_RGs, function(RG) "I" %in% RG)
@@ -223,14 +212,15 @@ xy_graph_frac <- apply(graph_frac_IL, 2, project2D)
 points(x = xy_graph_frac["x", ], y = xy_graph_frac["y", ],
        col = 1:length(MOIs), pch = 1, cex = 2)
 legend("bottom", bty = "n", pch = c(20, 1), pt.cex = c(1,2),
-       legend = c("Posterior probability", "Relative graph proportion"), )
+       legend = c("Posterior probability", "Posterior approximation"), )
+
+
+
 
 
 
 #===============================================================================
 # Recrudescence vs relapse using MOIs to change graphs
-#
-# When data are informative, model returns the prior on graphs re-weighted
 # ===============================================================================
 set.seed(1)
 marker_count <- 100; allele_count <- 10 # Data informativeness
@@ -248,7 +238,7 @@ MOIs <- list(c(1,1), c(2,1), c(3,1), c(2,2), c(3,2), c(3,3))
 # ------------------------------------------------------------------------------
 results <- sapply(MOIs, function(x) compute_posterior(y, fs, MOIs = x)$marg)
 xy <- apply(results, 2, project2D)
-plot_simplex(vertex_names, 0.5)
+plot_simplex(c(C = "Recrudescence", L = "Relapse", I = "Reinfection"), 0.5)
 points(x = xy["x", ], y = xy["y", ], pch = 20, col = 1:length(MOIs))
 legend("left", bty = "n", col = 1:length(MOIs), pch = 20, title = "MOIs",
        legend = sapply(MOIs, paste, collapse = " & "))
@@ -259,14 +249,17 @@ legend("left", bty = "n", col = 1:length(MOIs), pch = 20, title = "MOIs",
 graph_frac_CL <- sapply(1:length(MOIs), FUN = function(i) {
   stuff <- get_graph_stuff(MOIs[[i]])
   RGs_C <- sapply(stuff$CIL_gvn_RGs, function(RG) "C" %in% RG)
-  x <- c(frac_C = 1/sum(RGs_C), frac_L = 1/length(stuff$RGs), frac_I = 0)
+  # Common numerator sum(RGs_C * RGs_edges_within) drops out upon normalisation
+  x <- c(frac_C = 1/sum(RGs_C),
+         frac_L = 1/length(stuff$RGs),
+         frac_I = 0)
   z <- x/sum(x)
 })
 xy_graph_frac <- apply(graph_frac_CL, 2, project2D)
 points(x = xy_graph_frac["x", ], y = xy_graph_frac["y", ],
        col = 1:length(MOIs), pch = 1, cex = 2)
 legend("bottom", bty = "n", pch = c(20, 1), pt.cex = c(1,2),
-       legend = c("Posterior probability", "Relative graph proportion"), )
+       legend = c("Posterior probability", "Posterior approximation"), )
 
 
 # ------------------------------------------------------------------------------
@@ -283,6 +276,8 @@ result33  <- compute_posterior(y, fs, MOIs = MOIs, return.logp = TRUE)
 RGs <- sapply(result33$RGs, function(RG) RG_to_igraph(RG, gs, ts_per_gs))
 CIL_gvn_RGs <- sapply(RGs, compatible_rstrs, gs_per_ts = gs_per_ts) # states
 RGs_C_or_L <- sapply(CIL_gvn_RGs, function(CIL_gvn_RG) !("I" %in% CIL_gvn_RG))
+RGs_C <- sapply(CIL_gvn_RGs, function(CIL_gvn_RG) "C" %in% CIL_gvn_RG)
+logps <- sapply(result33$RGs, function(RG) RG$logp) # extract logp
 
 # Make a vector of intra-infection edges by first creating a block diag. matrix
 mat_within <- Matrix::bdiag(lapply(MOIs,function(x) matrix(1, ncol=x, nrow=x)))
@@ -290,13 +285,13 @@ colnames(mat_within) <- gs; rownames(mat_within) <- gs
 edges_within <- igraph::as_ids(igraph::E(igraph::graph_from_adjacency_matrix(mat_within, mode = "undirected", diag = F)))
 RGs_edges <- lapply(RGs, function(RG) igraph::as_ids(igraph::E(RG)))
 RGs_edges_within <- sapply(RGs_edges, function(RG_E) all(edges_within %in% RG_E))
-logps <- sapply(result33$RGs, function(RG) RG$logp) # extract logp
+
 
 # Inspect likelihoods:
 if (max(logps[!(RGs_C_or_L & RGs_edges_within)]) < min(logps[RGs_C_or_L & RGs_edges_within])) {
   writeLines("Expected-most-likely graphs are most likely ")
   for(i in which(RGs_C_or_L & RGs_edges_within)) plot_RG(RGs[[i]])
-  high_logps <- table(logps[RGs_C_or_L & RGs_edges_within])
+  high_logps <- table(logps[RGs_C & RGs_edges_within])
   writeLines(sprintf("%s likelihood values among %s graphs", length(high_logps), sum(high_logps)))
   print(high_logps)
 }
