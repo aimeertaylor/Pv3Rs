c299b5bdb73c190fc8106adeea09d179f334598f
aimeertaylor <aimee.r.taylor.85@gmail.com>
2025-03-12 11:10:45 +0100

    Remove non-exact sim results and update

--- DevFiles/maxima_with_graph_size.R
+++ DevFiles/maxima_with_graph_size.R
@@ -5,10 +5,8 @@
 # script feature in graph-prior.tex.
 ################################################################################
 rm(list = ls())
-load("../data/maxima.rda")
 all_MOIs <- sapply(colnames(maxima), function(x) as.numeric(strsplit(x, split = "")[[1]]))
 maxMOIs <- sapply(all_MOIs, function(x) max(x))
-logit <- function(x) log(x/(1-x))
 
 # ==============================================================================
 # General understanding of maximum probabilities
@@ -21,27 +19,26 @@ MOI1s_log <- sapply(all_MOIs, function(x) all(x == 1)) # MOI vectors of all 1s
 Episode_counts <- sort(unique(nMOI))
 
 # Cases where the MOI is always one
-plot(x = nMOI[MOI1s_log], y = maxima["theory_C_with", MOI1s_log], bty = "n",
+plot(x = nMOI[MOI1s_log], y = maxima["C_with", MOI1s_log], bty = "n",
      type = "b", main = "", bg = "yellow", pch = 24, ylim = c(0.5, 1),
      xlab = "Number of monoclonal episodes",
      ylab = "Maximum probability of first recurrence")
-lines(x = nMOI[MOI1s_log], y = maxima["theory_I_with", MOI1s_log],
+lines(x = nMOI[MOI1s_log], y = maxima["I_with", MOI1s_log],
       type = "b", bg = "red", pch = 25)
 legend("top", legend = c("Recrudescence", "Reinfection"), pch = c(24, 25),
        bt = "n", pt.bg = c("red", "yellow"))
 
 # Imbalance between episodes one and two
-
-plot(x = MOI12diff, y = maxima["theory_C_with", ], ylim = c(0.5, 1), bty = "n",
+plot(x = MOI12diff, y = maxima["C_with", ], ylim = c(0.5, 1), bty = "n",
      col = MOI1, pch = as.character(nMOI),
      ylab = "Maximum probability of recrudescence",
-     xlab = "MOI episode 1 - MOI episode 2"
+     xlab = "MOI episode 1 - MOI episode 2")
 text(y = 0.5, x = 2, adj = 1,
      labels = paste0("Number of episodes: ", paste(Episode_counts, collapse = " ")))
 legend("left", title = "MOI of episode 1", bty = "n",
        legend = unique(MOI1), fill = unique(MOI1))
 
-plot(x = MOI12diff, y = maxima["theory_I_with", ], ylim = c(0.5, 1), bty = "n",
+plot(x = MOI12diff, y = maxima["I_with", ], ylim = c(0.5, 1), bty = "n",
      col = MOI1, pch = as.character(nMOI), ylab = "Maximum probability of reinfection",
      xlab = "MOI episode 1 - MOI episode 2")
 text(y = 0.5, x = 2, adj = 1,
@@ -53,7 +50,7 @@ legend("right", title = "MOI of episode 1", bty = "n",
 # Trend is the same for both recrudescence and reinfection
 # (number of potentially independent inter-episode edges for episodes 1 & 2)
 plot(x = MOI2[MOI12diff == 0], bty = "n",
-     y = maxima["theory_C_with", ][MOI12diff == 0],
+     y = maxima["C_with", ][MOI12diff == 0],
      ylim = c(0.5, 1),
      col = MOI1[MOI12diff == 0], pch = as.character(nMOI[MOI12diff == 0]),
      xlab = "MOI of episodes 1 = 2", ylab = "Maximum probability of recrudescence")
@@ -63,7 +60,7 @@ legend("right", title = "MOI of episode 1", fill = unique(nMOI[MOI12diff == 0]),
        legend = unique(nMOI[MOI12diff == 0]), bty = "n",)
 
 plot(x = MOI2[MOI12diff == 0], bty = "n",
-     y = maxima["theory_I_with", ][MOI12diff == 0],
+     y = maxima["I_with", ][MOI12diff == 0],
      ylim = c(0.5, 1),
      col = MOI1[MOI12diff == 0], pch = as.character(nMOI[MOI12diff == 0]),
      xlab = "MOI of episodes 1 = 2", ylab = "Maximum probability of reinfection")
