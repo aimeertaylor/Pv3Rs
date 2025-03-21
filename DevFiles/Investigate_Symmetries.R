c8eaa951165af8407996a18069f23340f1d4e041
Yong See Foo <yongsee0819@yahoo.com>
2025-03-21 18:13:11 +1100

    more cases where most likely RG is incompatible with most likely recurrence seq

--- DevFiles/Investigate_Symmetries.R
+++ DevFiles/Investigate_Symmetries.R
@@ -32,3 +32,23 @@ plot_RG(RG_to_igraph(RG_mode, gs, ts_per_gs), edge.curved=0.25, vertex.size=20)
 # most likely RG has all siblings edges, only graph in its equivalence class
 # compare this with the RG where g1 and g2 are siblings, g4 is a clone of g1
 # this RG is 'intuitively' the most likely when all permutations are considered (there are 6)
+
+
+
+# The phenomenon that the most likely RG is not necessarily compatible with the
+# most likely recurrence sequence can also occur when MOIs match observed diversity
+y <- list(enroll = list(m1=c("A","B")), recur1 = list(m1=c("A")), recur2 = list(m1=c("B")))
+MOIs <- c(2,1,1)
+alleles <- c("A","B","C")
+gs_per_ts <- split(paste0("g", 1:sum(MOIs)), rep(1:length(MOIs), MOIs))
+# Fix data, alter allele frequencies f
+# Print seeds whose corresponding f results in the phenomenon above
+# Have not analysed the reasons behind the incompatibility
+for(s in 1:50) {
+  set.seed(s)
+  fs <- list(m1=setNames(gtools::rdirichlet(1, rep(0.5, length(alleles)))[1,], alleles))
+  res <- suppressMessages(compute_posterior(y, fs, MOIs=MOIs, return.logp=T))
+  post_r <- names(which.max(res$joint))
+  RG_mode <- res$RGs[[which.max(sapply(res$RGs, function(RG) RG$logp))]]
+  if(post_r %in% compatible_rstrs(RG_mode, gs_per_ts) == F) print(s)
+}
