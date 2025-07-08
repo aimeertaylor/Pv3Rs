a9ee5c9984a6b58523ea629e52f6db690fe42b22
aimeertaylor <aimee.r.taylor.85@gmail.com>
2025-07-08 15:09:40 +0200

    Update

--- R/data.R
+++ R/data.R
@@ -1,18 +1,21 @@
-#' Example Plasmodium vivax data
+#' Example *Plasmodium vivax* data
 #'
-#' Previously-published microsatellite data on Plasmodium vivax parasites
-#' extracted from patients enrolled in the Best Primaquine Dose (BPD) and Vivax
+#' Previously-published microsatellite data on *P. vivax* parasites
+#' extracted from study participants enrolled in the Best Primaquine Dose (BPD) and Vivax
 #' History (VHX) trials; see
-#' <https://www.nature.com/articles/s41467-019-13412-x>.
+#' [Taylor & Watson et al. 2019](https://www.nature.com/articles/s41467-019-13412-x)
+#' for more details of the genetic data; for more details of the trials, see
+#' [Chu et al. 2018a](https://doi.org/10.1093/cid/ciy319) and
+#' [Chu et al. 2018b](https://doi.org/10.1093/cid/ciy735).
 #'
-#' @format A list of 217 patients; for each patient, a list of one or more
+#' @format A list of 217 study participants; for each study participant, a list of one or more
 #' episodes; for each episode, a list of three or more microsatellite markers;
 #' for each marker, a vector of observed alleles (repeat lengths). For example:
 #' \describe{
-#'   \item{BPD_103}{Patient identifier: patient 103 in the BPD trial}
-#'   \item{BPD_103_1}{Episode identifier: episode one of patient 103 in the BPD trial}
-#'   \item{PV.3.27}{Marker identifier: Plasmodium vivax 3.27}
-#'   \item{18}{Repeat length: 18}
+#'   \item{BPD_103}{Study participant identifier: study participant 103 in the BPD trial}
+#'   \item{BPD_103_1}{Episode identifier: episode one of study participant 103 in the BPD trial}
+#'   \item{PV.3.27}{Marker identifier: *P. vivax* 3.27}
+#'   \item{18}{Allele identifier: 18 repeat lengths}
 #' }
 #'
 #' @source
@@ -22,11 +25,11 @@
 
 
 
-#' Allele frequencies computed from example Plasmodium vivax data
+#' Allele frequencies computed using example *Plasmodium vivax* data
 #'
 #' The posterior mean of a multinomial-Dirichlet model with uniform prior fit to
-#' data on allele prevalence in initial episodes. Because the model is fit to
-#' allele prevalence (observed) not allele frequency (would requires
+#' data on allele prevalence in initial episodes of [ys_VHX_BPD]. Because the
+#' model is fit to allele prevalence (observed) not allele frequency ( requires
 #' integrating-out unknown multiplicities of infection) it is liable to
 #' underestimate the frequencies of common alleles and overestimate those of
 #' rare but detected alleles.
@@ -43,36 +46,39 @@
 
 #' Upper bounds on posterior probabilities
 #'
-#' Upper bounds on posterior probabilities of first recurrence recrudescence and
+#' Upper bounds on posterior probabilities of 1st-recurrence recrudescence and
 #' reinfection (rows) for various MOI vectors (columns) that sum to at most
-#' eight. For a single recurrence (MOI vectors of length two), upper bounds are
+#' eight.
+#' #' These upper bounds feature in
+#' [vignette("understand-graph-prior")](../doc/understand-graph-prior.pdf) and
+#' [vignette("intra-episode-siblings")](../doc/intra-episode-siblings.html).
+#' For a single recurrence (MOI vectors of length two), upper bounds are
 #' induced by the prior. For more than one recurrence (MOI vectors of length
 #' three or more), upper bounds assume all but the first recurrence has data;
-#' these bounds are only valid for monoclonal episodes. Two upper bounds for
-#' each recurrence state are reported: one where summation over graphs includes
-#' graphs with cliques of three or more intra-episode siblings, another where
-#' summation over graphs excludes graphs with cliques of three or more
-#' intra-episode siblings. Since graphs with cliques of three or more
+#' these bounds are only valid for monoclonal episodes; see XXX. Two upper
+#' bounds for each recurrence state are reported: one where summation over
+#' graphs includes graphs with cliques of three or more intra-episode siblings,
+#' another where summation over graphs excludes graphs with cliques of three or
+#' more intra-episode siblings. Since graphs with cliques of three or more
 #' intra-episode siblings only apply when MOI vectors include MOIs of three or
 #' more, these two bounds only differ for MOI vectors that include MOIs of three
-#' or more (see example below). The column names represent MOI vectors whose
-#' MOIs are all less than or equal to eight.
+#' or more; see **Examples**.
 #'
 #' @format
 #' A matrix with 4 rows and 247 columns:
 #' \describe{
-#'   \item{C_with}{Upper bound on the posterior probability of recrudescence
-#'   when summation includes graphs with cliques of three or more intra-episode siblings}
-#'   \item{C_wout}{Upper bound on the posterior probability of recrudescence
-#'   when summation excludes graphs with cliques of three or more intra-episode siblings}
-#'   \item{I_with}{Upper bound on the posterior probability of reinfection when
-#'   summation includes graphs with cliques of three or more intra-episode siblings}
-#'   \item{I_wout}{Upper bound on the posterior probability of reinfection when
-#'   summation excludes graphs with cliques of three or more intra-episode siblings}
-#'   \item{11}{MOI vector (1, 1)}
-#'   \item{12}{MOI vector (1, 2)}
+#'   \item{C_with}{Row name; upper bound on the posterior probability of recrudescence
+#'   when summation includes graphs with cliques of three or more intra-episode siblings.}
+#'   \item{C_wout}{Row name; upper bound on the posterior probability of recrudescence
+#'   when summation excludes graphs with cliques of three or more intra-episode siblings.}
+#'   \item{I_with}{Row name; upper bound on the posterior probability of reinfection when
+#'   summation includes graphs with cliques of three or more intra-episode siblings.}
+#'   \item{I_wout}{Row name; upper bound on the posterior probability of reinfection when
+#'   summation excludes graphs with cliques of three or more intra-episode siblings.}
+#'   \item{11}{Column name; MOI vector (1, 1).}
+#'   \item{12}{Column name; MOI vector (1, 2).}
 #'   ...
-#'   \item{11111111}{MOI vector (1, 1, 1, 1, 1, 1, 1, 1)}
+#'   \item{11111111}{Column name; MOI vector (1, 1, 1, 1, 1, 1, 1, 1).}
 #' }
 #' @source <https://github.com/aimeertaylor/Pv3Rs/blob/main/data-raw/maxima.R>
 #'
