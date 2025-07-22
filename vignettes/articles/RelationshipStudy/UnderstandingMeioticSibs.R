481b0be46826c21e598d57e1c3f7502a2b89aee2
Aimee Taylor <aimee.r.taylor.85@gmail.com>
2025-07-22 11:03:17 +0200

    Add favicon; don't ignore source .R; update paths to RelationshipStudy

---
+++ vignettes/articles/RelationshipStudy/UnderstandingMeioticSibs.R
@@ -0,0 +1,82 @@
+################################################################################
+# Script illustrating that perfect bulk data from a pool of three meiotic
+# siblings is indistinguishable from perfect bulk data from their two parents;
+# that a single error is enough to recover full-sibling like behaviour; and that
+# meiotic siblings are 33% related.
+#
+# See following for examples of complex chiasmata
+# https://biology.stackexchange.com/questions/42288/do-only-one-or-both-pairs-of-homologous-chromatids-exchange-genetic-material-dur
+################################################################################
+rm(list = ls())
+library(Pv3Rs)
+library(MCMCpack)
+max_n_markers <- 150
+all_markers <- paste0("m", 1:max_n_markers)
+n_alleles <- 5
+alleles <- letters[1:n_alleles]
+fs_param <- setNames(rep(1, n_alleles), alleles)
+fs <- sapply(all_markers, function(i) {
+  setNames(rdirichlet(1, fs_param), alleles)}, simplify = F)
+parent1 <- sapply(all_markers, function(i) sample(alleles, size = 1, prob = fs[[i]]))
+parent2 <- sapply(all_markers, function(i) sample(alleles, size = 1, prob = fs[[i]]))
+parents <- cbind(parent1, parent2)
+chrs_per_marker <- round(seq(0.51, 14.5, length.out = max_n_markers))
+cs_meiotic <- Pv3Rs:::recombine_parent_ids(chrs_per_marker)
+cs_full <- sapply(1:4, function(i) Pv3Rs:::recombine_parent_ids(chrs_per_marker)[,1])
+
+# Construct children genotypes from parental allocations
+children_meiotic <- sapply(1:max_n_markers, function(i) {
+  sapply(1:ncol(cs_meiotic), function(j) parents[i,cs_meiotic[i,j]])
+})
+colnames(children_meiotic) <- all_markers
+
+# Construct children genotypes from parental allocations
+children_full <- sapply(1:max_n_markers, function(i) {
+  sapply(1:ncol(cs_full), function(j) parents[i,cs_full[i,j]])
+})
+colnames(children_full) <- all_markers
+
+# Format parasite infection data for compute_posterior
+y_full <- list(initial = apply(children_full[1:3,], 2, unique, simplify = F),
+                   relapse = apply(children_full[4,,drop=FALSE], 2, unique, simplify = F))
+
+y_meitotic <- list(initial = apply(children_meiotic[1:3,], 2, unique, simplify = F),
+                   relapse = apply(children_full[4,,drop=FALSE], 2, unique, simplify = F))
+
+y_parents <- list(initial = apply(t(parents), 2, unique, simplify = F),
+                  relapse = apply(children_full[4,,drop=FALSE], 2, unique, simplify = F))
+
+# Meiotic sibs and their parents indistinguishable:
+suppressMessages(compute_posterior(y = y_parents, fs)$marg)
+suppressMessages(compute_posterior(y = y_meitotic, fs)$marg)
+
+# Providing the true but unknowable MOI makes little difference
+suppressMessages(compute_posterior(y = y_meitotic, fs, MOIs = c(3,1))$marg)
+
+# Because we assume no error, a single error is enough to solve the problem:
+y_meitotic$initial$m150 <- "a"
+suppressMessages(compute_posterior(y = y_meitotic, fs)$marg)
+
+# No problem with full siblings:
+suppressMessages(compute_posterior(y = y_full, fs)$marg)
+
+# Plot data:
+plot_data(list(meiotic = y_meitotic,
+               parents = y_parents,
+               full = y_full), marker.annotate = F)
+
+# Check siblings are related as expected:
+all_pairs <- gtools::combinations(4,2)
+
+average_meiotic_relatedness <- mean(apply(all_pairs, 1, function(ind){
+  pair <- cs_meiotic[,ind]
+  mean(pair[,1] == pair[,2])
+}))
+
+average_full_relatedness <- mean(apply(all_pairs, 1, function(ind){
+  pair <- cs_full[,ind]
+  mean(pair[,1] == pair[,2])
+}))
+
+average_meiotic_relatedness
+average_full_relatedness
---
+++ vignettes/articles/RelationshipStudy/enumerate_halfsib_alleles.R
@@ -0,0 +1,51 @@
+# List all allelic draws for three half siblings
+#
+# Given a specified number of alleles for a single marker,
+# `generate_halfsib_alleles()` enumerates all the ways three half
+# siblings can draw alleles from their respective parents by firstly
+# enumerating all allelic combinations for the parents, and by secondly
+# enumerating all the inheritable combinations for the children.
+#
+# @param n_alleles Positive whole number specifying a per-marker number of
+#   alleles, otherwise known as marker cardinality.
+#
+# @returns A character matrix. Each column is an individual. Each row is a
+#   possible allelic draw. Alleles are represented by the first `n_alleles`
+#   letters of the latin alphabet.
+# @examples
+# enumerate_halfsib_alleles(3)
+
+enumerate_halfsib_alleles <- function(n_alleles){
+
+
+  # Check positive integer
+  if(!(n_alleles > 0 & is_wholenumber(n_alleles))) {
+    stop ("n_alleles should be a positive wholenumber")
+  }
+
+  # Per-marker alphabet
+  alleles <- LETTERS[1:n_alleles]
+
+  # Generate all permutations of parental alleles
+  parental_alleles <- as.matrix(expand.grid(p1 = alleles,
+                                            p2 = alleles,
+                                            p3 = alleles,
+                                            stringsAsFactors = FALSE))
+
+  # Generate all permutations of inheritance
+  child_parents <- as.matrix(expand.grid(child12 = c("p1", "p2"),
+                                         child13 = c("p1", "p3"),
+                                         child23 = c("p2", "p3"),
+                                         stringsAsFactors = FALSE))
+
+  # Extract inherited alleles
+  child_alleles <- do.call(rbind, apply(child_parents, 1, function(ps) {
+    parental_alleles[,ps]
+  }, simplify = F))
+
+  # Rename
+  colnames(child_alleles) <- paste("Half sibling", 1:3)
+
+  # End of function
+  return(child_alleles)
+}
